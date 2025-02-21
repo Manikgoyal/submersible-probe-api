08671f65b56dda9dc84b38a46b3d1c897a8a4afd
manik <manik@techdazzler.com>
2025-02-21 21:50:02 +0530

    Initialize project with base structure

---
+++ src/main/java/com/example/probe/SubmersibleProbeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.probe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmersibleProbeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmersibleProbeApiApplication.class, args);
+    }
+
+}
